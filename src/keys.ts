1b3f5b424c34636b72f5c464579eefa5d511818f
jiyuu-jin <zach@valist.io>
2025-01-07 22:33:08 -0500

    Lazy import of keytar

--- src/keys.ts
+++ src/keys.ts
@@ -1,18 +1,18 @@
-import keytar from 'keytar';
-import * as inquirer from 'inquirer';
-
 export async function select() {
-  const credentials = await keytar.findCredentials('VALIST')
+  const keytar = await import('keytar').then(module => module.default || module);
+  const inquirer = await import('inquirer');
+
+  const credentials = await keytar.findCredentials('VALIST');
   if (credentials.length === 0) {
-    throw new Error('no accounts found. use import to add an account.')
+    throw new Error('No accounts found. Use import to add an account.');
   }
 
   const { account } = await inquirer.prompt([{
     name: 'account',
-    message: 'select an account',
+    message: 'Select an account',
     type: 'list',
     choices: credentials.map(c => ({ name: c.account, value: c.password })),
   }]);
 
-  return account
+  return account;
 }
